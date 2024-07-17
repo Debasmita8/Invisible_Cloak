e19b41c4076c60702cd3c16542c8a8d585895e07
Debasmita8 <140266871+Debasmita8@users.noreply.github.com>
2024-07-17 10:18:10 +0530

    Update Invisible_Cloak.py

--- Invisible_Cloak.py
+++ Invisible_Cloak.py
@@ -4,62 +4,45 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-# Store a single frame as background 
-background = cap.read()
+_, bg = cap.read()
 time.sleep(2)
-background = cap.read()
-
-#define all the kernels size  
+_, bg = cap.read()
+ 
 open_kernel = np.ones((5,5),np.uint8)
 close_kernel = np.ones((7,7),np.uint8)
 dilation_kernel = np.ones((10, 10), np.uint8)
 
-# Function for remove noise from mask 
 def filter_mask(mask):
 
     close_mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, close_kernel)
-
     open_mask = cv2.morphologyEx(close_mask, cv2.MORPH_OPEN, open_kernel)
-
     dilation = cv2.dilate(open_mask, dilation_kernel, iterations= 1)
 
     return dilation
 
 while cap.isOpened():
-    ret, frame = cap.read()  # Capture every frame
-    # convert to hsv colorspace 
+    ret, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # lower bound and upper bound for Green color 
-    lower_bound = np.array([50, 80, 50])     
-    upper_bound = np.array([90, 255, 255])
+    # lower bound and upper bound for black color 
+    lower_bound = np.array([0, 0, 0])
+    upper_bound = np.array([180, 255, 30])
     
-
-    # find the colors within the boundaries
     mask = cv2.inRange(hsv, lower_bound, upper_bound)
-
-    # Filter mask
     mask = filter_mask(mask)
 
-    # Apply the mask to take only those region from the saved background 
-    # where our cloak is present in the current frame
-    cloak = cv2.bitwise_and(background, background, mask=mask)
+    cloak = cv2.bitwise_and(bg, bg, mask=mask)
 
-    # create inverse mask 
     inverse_mask = cv2.bitwise_not(mask)  
 
-    # Apply the inverse mask to take those region of the current frame where cloak is not present 
-    current_background = cv2.bitwise_and(frame, frame, mask=inverse_mask)
+    current_bg = cv2.bitwise_and(frame, frame, mask=inverse_mask)
 
-    # Combine cloak region and current_background region to get final frame 
-    combined = cv2.add(cloak, current_background)
+    combined = cv2.add(cloak, current_bg)
 
     cv2.imshow("Final output", combined)
-
 
     if cv2.waitKey(1) == ord('q'):
         break
     
 cap.release()
 cv2.destroyAllWindows()
-
